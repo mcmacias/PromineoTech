e4bf3c6659d0282b7b3b2e9195b769081c1462e0
Mayra C Macias <mayracarimaci@gmail.com>
2023-06-29 09:27:28 -0700

    finished assignment

--- Week-11-JQuery_and_AJAX/Assignment/tictactoe.js
+++ Week-11-JQuery_and_AJAX/Assignment/tictactoe.js
@@ -1,33 +1,26 @@
 
-function gameReset(grid, board){
+function gameReset(){
     //make button appear
-    const btn = $('#announceWinner').after('<button id="resetBtn">Reset Game</button>');
+    // const btn = $('#announceWinner').after('<button id="resetBtn">Reset Game</button>');
     
     $('#resetBtn').on("click", function(e){
-        console.log(grid);
-        console.log(board);
 
-        for(let i = 0; i < board.length; ++i){
-            board[i] = '';
-            console.log('this worked');
+        for(let i = 1; i < 10; ++i){
+            let $cell = $(`#c${i}`);
+            if($cell.hasClass('full')){
+                $cell.removeClass('full');
+                $cell.empty();
+            }
         }
-        console.log(board);
-
-        for(let i = 0; i < grid.length; ++i){
-            console.log(grid[0].classList);
-        }
+        document.getElementById('announceWinner').innerHTML = ('');
         
     });
-    // console.log(grid);
-    // console.log(board);
-    // console.log(grid[0].classList)
 
 }
 
 function checkWinner(Player, item1, item2){
     let grid = document.querySelectorAll('.cell')    
     let board = Array.from(grid).map(cell => cell.innerHTML);
-    // console.log(board);
 
     const winningCombos = [
         //horizontal
@@ -43,30 +36,12 @@ function checkWinner(Player, item1, item2){
         [2,4,6],
     ];
 
-    // winningCombos.forEach(combo => {
-    //     if (board[combo[0]] == item1 && board[combo[1]] == item1 && board[combo[2]] == item1){
-    //         // console.log(`Player ${Player+1} wins!`);
-    //         document.getElementById('announceWinner').innerHTML = (`Player 1 wins!`);
-    //         document.getElementById('board').style.pointerEvents = 'none';
-    //         gameReset(grid, board);
-    //     }else if (board[combo[0]] == item2 && board[combo[1]] == item2 && board[combo[2]] == item2){
-    //         // console.log(`Player ${Player+1} wins!`);
-    //         document.getElementById('announceWinner').innerHTML = (`Player 2 wins!`);
-    //         gameReset(grid, board);
-    //     }else{
-    //         document.getElementById('announceWinner').innerHTML = (`no winner`);
-    //         gameReset(grid, board);
-    //     }
-    // })
-
-
     if(Player == 0){ //if player 1, check item 1
         winningCombos.forEach(combo => {
             if (board[combo[0]] == item1 && board[combo[1]] == item1 && board[combo[2]] == item1){
                 // console.log(`Player ${Player+1} wins!`);
                 document.getElementById('announceWinner').innerHTML = (`Player ${Player+1} wins!`);
-                document.getElementById('board').style.pointerEvents = 'none';
-                gameReset(grid, board);
+                // document.getElementById('board').style.pointerEvents = 'none';
             }
         })
     }else if(Player == 1){ //if player 2, check item 2
@@ -74,29 +49,12 @@ function checkWinner(Player, item1, item2){
             if (board[combo[0]] == item2 && board[combo[1]] == item2 && board[combo[2]] == item2){
                 // console.log(`Player ${Player+1} wins!`);
                 document.getElementById('announceWinner').innerHTML = (`Player ${Player+1} wins!`);
-                gameReset(grid, board);
             }
         })
     }else{ //need 
         document.getElementById('announceWinner').innerHTML = (`no winner`);
-        gameReset(grid, board);
     }
 
-    
-    /**
-     * check for an actual winner since we have our game board...
-     * 
-     * horizontal
-     * vertical
-     * diagonal
-     * 
-     * look into the .every() method if you want a 1 liner
-     * you can also use 98 if statements
-     * or, consolidate to less using if/else
-     * 
-     * array of winning combos? 
-     * 
-     */
 }
 
 (function($, undefined) {
@@ -112,7 +70,6 @@ function checkWinner(Player, item1, item2){
   
       $game.on('click', '.cell', (e) =>{
         const $cell = $(e.target).closest('.cell');
-        // console.log($cell);
         if(!$cell.hasClass('full')){
             if(currentPlayer === 0) { //check which player is playing 
             $cell.append(playerO);
@@ -129,6 +86,7 @@ function checkWinner(Player, item1, item2){
             return null;
         }
       });
+      gameReset();
     });  
 
     
