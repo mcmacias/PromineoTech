f83cbcd77369abf89bcf4f18348723ac3d3cd7e9
Mayra C Macias <mayracarimaci@gmail.com>
2023-08-03 20:24:08 -0700

    readBooks array

--- final-project/src/Home.js
+++ final-project/src/Home.js
@@ -1,2 +1,23 @@
 //Home page setup, include nav bar & links to Read & Tbr in body
 //app that will run everything in index.js
+import './Home.css';
+import NavBar from './NavBar.js';
+import ReadBooks from './ReadBooks.js';
+import TBR from './TBR.js'
+
+function Home (){
+  return(
+    <div>
+      {/* <NavBar/> */}
+      <h1>Book Tracker</h1>
+      <div className='container'>
+        {/* make these photo links and on click goes to this page  */}
+        <ReadBooks/>
+        {/* <TBR/> */}
+      </div>
+    </div>
+  )
+
+}
+
+export default Home;
--- final-project/src/ReadBooks.js
+++ final-project/src/ReadBooks.js
@@ -1,3 +1,47 @@
 //page that will display read books
 //Title, author, Dates read, star rating, mini review
 //get of read list
+import { useState, useEffect } from "react";
+
+const READ_URL = `https://64c5aeebc853c26efadaea42.mockapi.io/PTF/ReadBooks`;
+let readBooks = [];
+
+export default function ReadBooks(props){
+
+  const [booksRead, setBooksRead] = useState([]);
+
+  useEffect(() => {
+    fetch(READ_URL)
+    .then(data => data.json())
+    .then(data => {
+      // console.log(data)
+      setBooksRead(data);
+      console.log(booksRead);
+    });
+  }, []);
+
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Dates Read</th>
+          <th>Rating</th>
+          <th>Review</th>
+        </tr >
+      </thead>
+      <tbody>
+        {booksRead.map((book, index) => {
+          <tr>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.date}</td>
+            <td>{book.rating}</td>
+            <td>{book.review}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+  );
+}
